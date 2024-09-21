5b48f372b59e9a9f98e00f04fbb715357e4026cf
Itsfoss0 <akhonyaebenezer@gmail.com>
2024-09-22 00:06:20 +0300

    [feat]: Add API integration for support tickets
    Summary:
    Create an endpoint /api/v1/support that creates a support ticket
    when a POST request is sent. No authentication or rate limiting checks
    have been added at this point

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,7 @@ const errorHandler = require('./middleware/errorHandler.middleware');
 const logger = require('./middleware/log.middleware');
 const authRouter = require('./auth/auth.route');
 const statusRouter = require('./routes/status.route');
+const supportRouter = require('./routes/support.route.js');
 
 const app = express();
 app.use(cors());
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(logger);
 app.use('/api/v1/status/', statusRouter);
 app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/support', supportRouter);
 
 // the error handler has to be the very last middleware
 app.use(errorHandler);
