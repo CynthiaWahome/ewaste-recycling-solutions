da76d6e37e5001716177ce37e75cc08bcb21d046
cynthia <cynthiaawsajira@gmail.com>
2024-09-25 01:59:16 +0300

    [feat]: Add blog, FAQ, Terms/Conditions, Privacy Policy

---
+++ frontend/src/pages/FAQ.page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../components/header/Header.component'
+import Footer from '../components/footer/Footer.component'
+
+const FAQ = () => {
+    return (
+        <div>
+            <Header />
+            <Footer />
+        </div>
+    )
+}
+
+export default FAQ
---
+++ frontend/src/pages/PrivacyPolicy.pages.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../components/header/Header.component'
+import Footer from '../components/footer/Footer.component'
+
+const PrivacyPolicy = () => {
+    return (
+        <>
+            <Header />
+            <Footer />
+        </>
+
+    )
+}
+
+export default PrivacyPolicy
